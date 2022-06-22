30b75e6b89341d440742625cc02138d229c0b34c
kdvornichenko <gglade999@gmail.com>
2022-06-22 22:02:43 +0600

    Mobile menu fix

--- src/script.js
+++ src/script.js
@@ -1,40 +1,25 @@
 window.addEventListener('DOMContentLoaded', function () {
-
 	// BURGER
 
 	const mobMenu = document.getElementById('mobMenu')
-	const mobLeft = document.getElementById('mobLeft')
-	const mobRight = document.getElementById('mobRight')
 	const body = document.getElementById('body')
-
-	function zIndex() {
-		if (mobMenu.classList.contains('z-10')) {
-			setTimeout(function () {
-				mobMenu.classList.remove('z-10')
-			}, 500)
-			setTimeout(function () {
-				mobMenu.classList.add('z-[-100]')
-			}, 500)
-			console.log('qwe')
-		} else {
-			mobMenu.classList.remove('z-[-100]')
-			mobMenu.classList.add('z-10')
-		}
-	}
+	const main = document.getElementById('main')
+	const nav = document.getElementById('nav')
 
 	burger.classList.add('untoggle')
-	burger.addEventListener('click', function closeMenu() {
-		this.classList.toggle('toggled')
-		this.classList.toggle('unToggled')
-		mobMenu.classList.toggle('-right-[100%]')
+	burger.addEventListener('click', menuLinkClick)
+
+	function menuLinkClick() {
+		burger.classList.toggle('toggled')
+		burger.classList.toggle('unToggled')
+		mobMenu.classList.toggle('-right-full')
 		mobMenu.classList.toggle('right-0')
-		zIndex()
-		mobLeft.classList.toggle('-left-full')
-		mobLeft.classList.toggle('-left-4')
-		mobRight.classList.toggle('-right-full')
-		mobRight.classList.toggle('right-0')
 		body.classList.toggle('overflow-y-hidden')
-	})
+		main.classList.toggle('opacity-30')
+		main.classList.toggle('opacity-100')
+		nav.classList.toggle('opacity-30')
+		nav.classList.toggle('opacity-100')
+	}
 
 	// Прокрутка при клике
 
@@ -47,16 +32,14 @@ window.addEventListener('DOMContentLoaded', function () {
 	}
 
 	function onmenuLinkClick(e) {
+		menuLinkClick()
 		const menuLink = e.target
 		if (
 			menuLink.dataset.goto &&
 			document.querySelector(menuLink.dataset.goto)
 		) {
 			const gotoBlock = document.querySelector(menuLink.dataset.goto)
-			const gotoBlockValue =
-				gotoBlock.getBoundingClientRect().top +
-				scrollY -
-				document.querySelector('header').offsetHeight
+			const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY
 			window.scrollTo({
 				top: gotoBlockValue,
 				behavior: 'smooth',
@@ -64,33 +47,6 @@ window.addEventListener('DOMContentLoaded', function () {
 			e.preventDefault()
 		}
 	}
-
-	// // METRIC SECTION INCREMENTING COUNTER
-	// const counters = document.querySelectorAll('#counter')
-
-	// counters.forEach(counter => {
-	// 	counter.innerText = '0'
-
-	// 	const updateCounter = () => {
-	// 		const target = +counter.getAttribute('data-target')
-	// 		const c = +counter.innerText
-
-	// 		const increment = target / 100
-
-	// 		if (c < target) {
-	// 			counter.innerText = `${Math.ceil(c + increment)}`
-	// 			setTimeout(updateCounter, 5)
-	// 		} else {
-	// 			counter.innerText = target
-	// 		}
-	// 	}
-
-	// 	window.addEventListener('scroll', function () {
-	// 		if (this.scrollY >= 1100) {
-	// 			updateCounter()
-	// 		}
-	// 	})
-	// })
 
 	// FAQ ACCORDION
 	const accordion = document.getElementsByClassName('accordion-btn')
